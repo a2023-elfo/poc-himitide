56b1237146e1ee78a54e03eae95457e62a9c8b22
AnahiMongelos <mona3503@usherbrooke.ca>
2023-11-12 14:39:33 -0500

    code-de-base
    le capteur capte

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,34 @@
+/*
+Capteur d'humidité pour le robot jardinier
+Anahi Michelle Mongelos Toledo
+11 novembre 2023
+*/
+
+#include <stdio.h>
+#include <SoftwareSerial.h>
 #include <Arduino.h>
-#include <LibRobus.h>
+//valeurs d'humidite
+const int sec = 595;
+const int humide = 239;
 
 void setup() {
-  BoardInit();
-  Serial.begin(9600);
-  Serial.println("Hello World!");
+Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int valeurCapteur = analogRead(A0);
+
+  int pourcentageHumidite = map(valeurCapteur, humide, sec, 100, 0);
+
+   if (pourcentageHumidite >= 0 && pourcentageHumidite <= 100) 
+   {
+    Serial.print(pourcentageHumidite);
+    Serial.println("%");
+  } 
+  
+  else 
+  {
+    Serial.println("Erreur de lecture");
+  }
+  delay(1000); //delai d'une seconde avant la prochaine lecture 
 }
